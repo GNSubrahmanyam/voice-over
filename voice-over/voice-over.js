9bc35fb21c0bdb337f14b298f236430cc52630b9
G Naga Subrahmanyam <gnsubrahmanyam1@gmail.com>
2017-10-11 23:27:19 +0530

    Added voice-over.js and voice-over.min.js

---
+++ voice-over/voice-over.js
@@ -0,0 +1,93 @@
+/**
+ * voice-over.js
+ *
+ *
+ * @license  the MIT license
+ * @version 0.1
+ * @author  G Naga Subrahmanyam, https://github.com/GNSubrahmanyam
+ * @created 11-Oct-2017
+ * @link
+ *
+ *
+ **/
+
+/**
+ * Usage:
+ * <div class="voice-over" data-voice-over="Some Text">Some Text</div>
+ *
+ **/
+
+
+ //  Check for browser support
+ if ('speechSynthesis' in window) {
+   console.log('Your browser supports speech synthesis.');
+ } else {
+   console.log('Sorry your browser does not support speech synthesis.');
+ }
+
+ // getting getElementsByClassName voice-over
+ var voiceOver = document.getElementsByClassName('voice-over');
+
+
+ // voices select menu
+ var voiceSelect = document.getElementById('voices');
+
+
+ // Fetch the list of voices and populate the voice options.
+ function loadVoices() {
+   // Fetch the available voices.
+   var voices = speechSynthesis.getVoices();
+
+   // Loop through each of the voices.
+   voices.forEach(function(voice, i) {
+     // Create a new option element.
+     var option = document.createElement('option');
+
+     // Set the options value and text.
+     option.value = voice.name;
+     option.innerHTML = voice.name;
+
+     // Add the option to the voice selector.
+     voiceSelect.appendChild(option);
+   });
+ }
+
+ // Execute loadVoices.
+ loadVoices();
+
+ // Chrome loads voices asynchronously.
+ window.speechSynthesis.onvoiceschanged = function(e) {
+   loadVoices();
+ };
+
+
+ // Create a new utterance for the specified text and add it to
+ // the queue.
+ function speak(text) {
+   // Create a new instance of SpeechSynthesisUtterance.
+   var msg = new SpeechSynthesisUtterance();
+
+   // Set the text.
+   msg.text = text;
+
+   // Set the attributes.
+   msg.volume = 1; // 0 to 1
+   msg.rate = 0.8; // 0.1 to 10
+   msg.pitch = 1.2; //0 to 2
+
+   // If a voice has been selected, find the voice and set the
+   // utterance instance's voice attribute.
+   if (voiceSelect.value) {
+     msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+   }
+
+   // Queue this utterance.
+   window.speechSynthesis.speak(msg);
+ }
+
+ for (var i = 0; i < voiceOver.length; i++) {
+ voiceOver[i].addEventListener('click',function(){
+   var speechMsgInput = this.getAttribute("data-voice-over");
+       speak(speechMsgInput);
+     });
+ }
